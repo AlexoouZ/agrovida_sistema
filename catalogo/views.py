9c4519ecb06290ff63c24e6a5fbe500bb9b7d5d8
alexander <lalerivera712@gmail.com>
2026-09-11 18:16:10 -0300

    vista sin terminar con config de template y urls

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from . import data
 # Create your views here.
 
-def mostrar_productos(productos)
-    
-
-return render(,,)
+def mostrar_productos(productos):
+    nombre = 
+    "productos" = {
+        "nombre":nombre
+    }
+    return render(productos,'catalogo/index.html',"productos")
